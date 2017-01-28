87040c923b37e7e24f31b2f8db6d3330b43a7b64
mukai1976 <mukai@sansou.co.jp>
2017-01-28 18:17:08 +0900

    Revert "HEROES  ID:11 name変更"

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ export class Hero {
 }
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Mukai' },
+  { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
