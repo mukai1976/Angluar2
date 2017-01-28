74949a70929218cbf6c3c95b0c0947e76cd384ec
mukai1976 <mukai@sansou.co.jp>
2017-01-28 18:15:34 +0900

    HEROES  ID:11 name変更

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ export class Hero {
 }
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
+  { id: 11, name: 'Mr. Mukai' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
